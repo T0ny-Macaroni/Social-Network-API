83ec7054309c5ab545dceacc60f45366d7cbda63
TonyMacaroni <Tonygiuliani3502@gmail.com>
2023-09-09 04:11:49 -0500

    finished routes

---
+++ routes/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../controllers/thoughtController');
+
+router.get('/', getAllThoughts);
+
+router.get('/:id', getThoughtById);
+
+router.post('/', createThought);
+
+router.put('/:id', updateThought);
+
+router.delete('/:id', deleteThought);
+
+router.post('/:thoughtId/reactions', createReaction);
+
+router.delete('/:thoughtId/reactions/:reactionId', deleteReaction);
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../controllers/userController');
+
+router.get('/', getAllUsers);
+
+router.get('/:id', getUserById);
+
+router.post('/', createUser);
+
+router.put('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
+
+router.post('/:userId/friends/:friendId', addFriend);
+
+router.delete('/:userId/friends/:friendId', removeFriend);
+
+module.exports = router
